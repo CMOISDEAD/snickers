a8d44bae85c42e97b767304af38ed08cc3f826ea
camilo <camiloesteban661@gmail.com>
2024-07-28 14:03:37 -0500

    feat: add main text and setup basic config

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,19 @@ import { Model } from "./components/three/Model";
 
 function App() {
   return (
-    <div className="h-screen">
+    <div className="relative h-screen">
+      <img
+        src="https://i.pinimg.com/originals/b9/46/bf/b946bfda1684e33d35517d77e931b050.jpg"
+        alt="vans logo"
+        className="absolute top-5 left-5 w-20 z-10"
+      />
       <Model />
+
+      <ul className="absolute bottom-5 left-0 right-0 w-fit m-auto flex gap-10">
+        <li>Sophisticated</li>
+        <li>Stylish</li>
+        <li>Comfortable</li>
+      </ul>
     </div>
   );
 }
--- src/components/three/Model.tsx
+++ src/components/three/Model.tsx
@@ -1,24 +1,44 @@
-import { Canvas } from "@react-three/fiber";
+import { Vector3 } from "three";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { Suspense } from "react";
 import { Sneaker } from "./Sneaker";
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, Text } from "@react-three/drei";
 
 export const Model = () => {
   return (
-    <Canvas
-      className="h-full w-full"
-      gl={{ preserveDrawingBuffer: true }}
-      onCreated={({ gl }) => {
-        gl.setClearColor("#282828");
-      }}
-    >
+    <Canvas className="h-full w-full">
       <ambientLight intensity={5} />
       <directionalLight position={[10, 10, 10]} intensity={10} />
       <Suspense fallback={null}>
         <Sneaker />
+        <Quote />
+        <Rig />
         <PerspectiveCamera makeDefault position={[1, 2, 5]} fov={2} />
         <OrbitControls enableZoom={false} />
       </Suspense>
     </Canvas>
   );
+};
+
+const Quote = () => {
+  const { width } = useThree((state) => state.viewport);
+
+  return (
+    <Text
+      font="/fonts/Ki.ttf"
+      fontSize={width / 9}
+      color="black"
+      anchorX="center"
+      anchorY="middle"
+      fontWeight="bold"
+    >
+      Step Into Cool.
+    </Text>
+  );
+};
+
+const Rig = ({ v = new Vector3() }) => {
+  return useFrame(({ camera, pointer }) => {
+    camera.position.lerp(v.set(pointer.x / 2, pointer.y / 2, 6), 0.02);
+  });
 };
