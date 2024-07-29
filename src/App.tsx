72444b3f3941185d86e9abd581617896f974ea3d
camilo <camiloesteban661@gmail.com>
2024-07-29 10:45:16 -0500

    feat: update style

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
+import { useState } from "react";
 import { Model } from "./components/three/Model";
 
 function App() {
+  const [selected, setSelected] = useState<null | string>(null);
+
   return (
     <div className="relative h-screen">
-      <img
-        src="https://i.pinimg.com/originals/b9/46/bf/b946bfda1684e33d35517d77e931b050.jpg"
-        alt="vans logo"
-        className="absolute top-5 left-5 w-20 z-10"
-      />
-      <Model />
+      <a href="/">
+        <img
+          src="https://i.pinimg.com/originals/b9/46/bf/b946bfda1684e33d35517d77e931b050.jpg"
+          alt="vans logo"
+          className="absolute top-5 left-5 w-20 z-10"
+        />
+      </a>
+      <div className="absolute bottom-5 left-5 flex flex-col gap-2 w-48 bg-black text-white font-ki">
+        <p className="text-[0.6rem]">
+          <span>&ldquo;</span>
+          Unleash Your Urban Spirit with Our Vans-Inspired Sneakers. Designed
+          for the rebels, the dreamers, and the trailblazers. Experience
+          unmatched comfort and bold style with every step. Make your mark on
+          the streets.
+          <span>&rdquo;</span>
+        </p>
+        <p className="text-sm">{new Date().toLocaleString()}</p>
+      </div>
 
-      <ul className="absolute bottom-5 left-0 right-0 w-fit m-auto flex gap-10">
-        <li>Sophisticated</li>
-        <li>Stylish</li>
-        <li>Comfortable</li>
-      </ul>
+      <Model description={selected} />
+
+      <div className="absolute bottom-0 top-0 right-5 m-auto h-full flex flex-col justify-between gap-5 w-1/12 py-5">
+        <ul className="flex flex-col gap-10">
+          {["Sophisticated", "Stylish", "Comfortable"].map((item, i) => (
+            <li
+              key={i}
+              onMouseEnter={() => setSelected(item)}
+              onMouseLeave={() => setSelected("Modern")}
+              className="cursor-pointer"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+        <button className="bg-black text-white p-2 hover:bg-neutral-950 transition-colors shadow w-full">
+          Buy Now
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/three/Model.tsx
+++ src/components/three/Model.tsx
@@ -4,14 +4,18 @@ import { Suspense } from "react";
 import { Sneaker } from "./Sneaker";
 import { OrbitControls, PerspectiveCamera, Text } from "@react-three/drei";
 
-export const Model = () => {
+interface Props {
+  description: string | null;
+}
+
+export const Model = ({ description }: Props) => {
   return (
     <Canvas className="h-full w-full">
       <ambientLight intensity={5} />
       <directionalLight position={[10, 10, 10]} intensity={10} />
       <Suspense fallback={null}>
         <Sneaker />
-        <Quote />
+        <Quote description={description} />
         <Rig />
         <PerspectiveCamera makeDefault position={[1, 2, 5]} fov={2} />
         <OrbitControls enableZoom={false} />
@@ -20,20 +24,35 @@ export const Model = () => {
   );
 };
 
-const Quote = () => {
+const Quote = ({ description }: Props) => {
+  const color = "black";
   const { width } = useThree((state) => state.viewport);
 
   return (
-    <Text
-      font="/fonts/Ki.ttf"
-      fontSize={width / 9}
-      color="black"
-      anchorX="center"
-      anchorY="middle"
-      fontWeight="bold"
-    >
-      Step Into Cool.
-    </Text>
+    <>
+      <Text
+        font="/fonts/Ki.ttf"
+        fontSize={width / 10}
+        maxWidth={width / 3}
+        color={color}
+        anchorX="right"
+        anchorY="middle"
+        fontWeight="bold"
+      >
+        Step Into Cool.
+      </Text>
+      <Text
+        font="/fonts/Ki.ttf"
+        fontSize={width / 30}
+        maxWidth={width / 2}
+        color={color}
+        anchorX="center"
+        anchorY={0.07}
+        fontWeight="bold"
+      >
+        A {description || "Modern"} Design
+      </Text>
+    </>
   );
 };
 
